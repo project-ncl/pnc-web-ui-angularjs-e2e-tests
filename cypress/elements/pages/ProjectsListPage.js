e02d745a0b7c07671c9a36c7860f1ca5f9752382
DnsZhou <tozhou@redhat.com>
2022-06-16 22:07:57 +0100

    Fix Projects List page timeout issue

--- cypress/elements/pages/ProjectsListPage.js
+++ cypress/elements/pages/ProjectsListPage.js
@@ -6,7 +6,9 @@ class ProjectsListPage {
   }
 
   clickLinkByProjectName(projectName) {
+    cy.wait(2000);
     const field = cy.get("div.ng-scope > .form-control");
+    field.clear();
     field.type(projectName + `{enter}`);
     cy.wait(1000);
     cy.get("a").contains(projectName).click();
