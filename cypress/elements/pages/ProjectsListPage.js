27b9a1350f64f7ac2cf7f1ac1c0bf835456f09d7
Dennis Zhou <tozhou@redhat.com>
2022-07-19 13:31:50 +0100

    [NCL-7157] Fix TC2 click link issue for stage

--- cypress/elements/pages/ProjectsListPage.js
+++ cypress/elements/pages/ProjectsListPage.js
@@ -6,11 +6,13 @@ class ProjectsListPage {
   }
 
   clickLinkByProjectName(projectName) {
-    const field = cy.get("pnc-projects-list-page [placeholder='string | !string | s?ring | st*ng']");
+    const field = cy.get(
+      "pnc-projects-list-page [placeholder='string | !string | s?ring | st*ng']"
+    );
     field.clear();
     field.type(projectName + `{enter}`);
     cy.wait(1000);
-    cy.get("a").contains(projectName).click();
+    cy.get("pnc-project-link > a").contains(projectName).click();
   }
 }
 
